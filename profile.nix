e9b7093123554287971e3059cb505f1788ac95cf
zimbatm <zimbatm@zimbatm.com>
2019-02-16 22:45:00 +0100

    pin nixpkgs

--- profile.nix
+++ profile.nix
@@ -1,10 +1,16 @@
-with import <nixpkgs> {};
-buildEnv {
-  name = "mdsh-env";
-  paths = [
-    cargo
-    cargo-edit
-    rustc
-    rustfmt
-  ];
-}
+let
+  sources = import ./nix/sources.nix;
+  pkgs = import sources.nixpkgs {
+    config = {};
+    overlays = [];
+  };
+in
+  pkgs.buildEnv {
+    name = "mdsh-env";
+    paths = with pkgs; [
+      cargo
+      cargo-edit
+      rustc
+      rustfmt
+    ];
+  }
