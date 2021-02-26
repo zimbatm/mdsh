88c03e6328cd222828fb24fd5b41923c7b3126a2
zimbatm <zimbatm@zimbatm.com>
2021-02-26 22:34:09 +0100

    bump dependencies

--- flake.nix
+++ flake.nix
@@ -1,6 +1,5 @@
 {
   description = "mdsh - a markdown shell pre-processor";
-  edition = 201909;
   outputs = { self, nixpkgs }: {
     devShell.x86_64-linux = import ./shell.nix {
       pkgs = nixpkgs.legacyPackages.x86_64-linux;
