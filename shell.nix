c10fa3e0d2420899fb2ebdf544eef62485b6f84a
zimbatm <zimbatm@zimbatm.com>
2022-11-02 15:29:48 +0100

    add libiconv as a dev dependency

--- shell.nix
+++ shell.nix
@@ -1,11 +1,12 @@
-{ pkgs ? import (fetchTarball "https://github.com/NixOS/nixpkgs/archive/nixos-22.05.tar.gz") {} }:
+{ pkgs ? import (fetchTarball "https://github.com/NixOS/nixpkgs/archive/nixos-22.05.tar.gz") { } }:
 pkgs.mkShell {
   buildInputs = [
-    pkgs.gitAndTools.git-extras
-    pkgs.gitAndTools.pre-commit
-    pkgs.rustfmt
     pkgs.cargo
     pkgs.cargo.passthru.rustc
+    pkgs.gitAndTools.git-extras
+    pkgs.gitAndTools.pre-commit
+    pkgs.libiconv
+    pkgs.rustfmt
   ];
 
   shellHook = ''
