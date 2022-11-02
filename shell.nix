a5a660e8131c4a6d92e661dffefd4ab0c44c06ca
Domen Kožar <domen@dev.si>
2022-11-02 12:40:12 +0000

    pin nixpkgs

--- shell.nix
+++ shell.nix
@@ -1,4 +1,4 @@
-{ pkgs ? import <nixpkgs> {} }:
+{ pkgs ? import (fetchTarball "https://github.com/NixOS/nixpkgs/archive/nixos-22.05.tar.gz") {} }:
 pkgs.mkShell {
   buildInputs = [
     pkgs.gitAndTools.git-extras
